5122bc737494ed082dbda57896f372a897eb3d30
Caliodore <caliodorecharbonneau@gmail.com>
2025-09-10 18:44:53 -0500

    Smoother Rotation Testing with Comments
    Attempting to figure out how to use Quaternion.Slerp() to smooth rotation of player character, has all the commented out sections from failed bits.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,8 +8,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float playerJumpSpeed = 10.0f;
 
     Rigidbody playerRB;
+    Transform playerTransform;
 
     Vector3 movementVector;
+    Vector3 movementForwardsRotVector;
+    Vector3 animatorForwards;
+    Vector3 playerObjForwards;
+    Vector3 desiredVector;
+
+    Quaternion playerObjRot;
+    Quaternion animatorForwardsRot;
+    Quaternion movementForwardsRot;
+
+    public bool playerMoving;
 
     [SerializeField] Animator playerAnimatorController;
 
@@ -30,6 +41,12 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         playerRB.AddForce(movementVector * playerMoveSpeed, ForceMode.Acceleration);
+        if(playerMoving)
+        {
+            movementForwardsRot = Quaternion.LookRotation(movementVector, Vector3.up);
+            playerObjRot = Quaternion.LookRotation(transform.forward, Vector3.up);
+            transform.rotation = Quaternion.Slerp(playerObjRot, movementForwardsRot, (1.0f * Time.deltaTime));    
+        }
     }
 
     public void OnJump(InputAction.CallbackContext ctx)
@@ -44,13 +61,23 @@ public class PlayerMovement : MonoBehaviour
     { 
         Vector2 inputVector = ctx.ReadValue<Vector2>();
         movementVector = new Vector3(inputVector.x, 0, inputVector.y);
+        Debug.DrawRay(transform.position, movementVector, Color.green, 5000);
+        
+        desiredVector = Vector3.Slerp(transform.position, movementVector, (1.0f * Time.deltaTime)).normalized;
+        Debug.DrawRay(transform.position, desiredVector, Color.cyan, 5000);
+
+        //movementForwardsRot = Quaternion.LookRotation(movementVector, Vector3.up);
+        //movementForwardsRotVector = movementForwardsRot.eulerAngles;
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, guh, (1.0f * Time.deltaTime));
 
         if (ctx.performed)
-        { 
+        {
+            playerMoving = true;
             playerAnimatorController.SetBool("playerRunning", true);
         }
         else if (ctx.canceled)
-        { 
+        {
+            playerMoving = false;
             playerAnimatorController.SetBool("playerRunning", false);    
         }
     }
